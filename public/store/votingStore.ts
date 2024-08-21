2a95e99786ea602dde1f3ead9d9985b23d4956bc
Illya Gerasymchuk <illya@iluxonchik.me>
2024-08-21 21:12:26 +0100

    feat: improve voting page + network switching corrections

--- public/store/votingStore.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'nanostores';
-import { VotingStep } from '../models/VotingState';
-
-export const currentStep = atom<VotingStep>(VotingStep.ConnectWallet);
-export function updateStep(step: VotingStep) {
-    currentStep.set(step);
-  }
