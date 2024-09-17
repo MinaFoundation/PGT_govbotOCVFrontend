193837e939616e646daf80729617585772552d63
Illya Gerasymchuk <illya@iluxonchik.me>
2024-09-17 16:25:32 +0100

    feat: get proposal data via HTTP

--- src/models/Proposal.ts
+++ src/models/Proposal.ts
@@ -1,13 +1,13 @@
-import { Model, DataTypes } from 'sequelize';
-import sequelize from '../db/connection';
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../db/connection";
 
 export enum ProposalStatus {
-  DRAFT = 'DRAFT',
-  CONSIDERATION_PHASE = 'CONSIDERATION_VOTE',
-  DELIBERATION_PHASE = 'DELIBERATION_VOTE',
-  FUNDING_VOTING_PHASE = 'FUNDING_VOTE',
-  FUNDED = 'FUNDED',
-  CANCELLED = 'CANCELLED',
+  DRAFT = "DRAFT",
+  CONSIDERATION_PHASE = "CONSIDERATION_VOTE",
+  DELIBERATION_PHASE = "DELIBERATION_VOTE",
+  FUNDING_VOTING_PHASE = "FUNDING_VOTE",
+  FUNDED = "FUNDED",
+  CANCELLED = "CANCELLED",
 }
 
 export interface ProposalAttributes {
@@ -19,52 +19,6 @@ export interface ProposalAttributes {
   fundingRoundId: number | null;
   forumThreadId: string | null;
   status: ProposalStatus;
+  createdAt: string;
+  updatedAt: string;
 }
-
-export class Proposal extends Model {
-}
-
-Proposal.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING(32),
-      allowNull: false,
-    },
-    proposerDuid: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    budget: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-    },
-    uri: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    fundingRoundId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    status: {
-      type: DataTypes.ENUM(...Object.values(ProposalStatus)),
-      allowNull: false,
-      defaultValue: ProposalStatus.DRAFT,
-    },
-    forumThreadId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'proposals',
-  }
-);
-
-export default Proposal;
